aa245df515609249caafd65d3c57a84be7fe5b21
Stephanie V <stephanievillarino@gmail.com>
2015-02-12 01:09:47 -1000

    added intents to switch screen to chosen play mode

--- app/src/main/java/ada/bathroomwall/StartScreen.java
+++ app/src/main/java/ada/bathroomwall/StartScreen.java
@@ -1,10 +1,12 @@
 package ada.bathroomwall;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -48,7 +50,9 @@ public class StartScreen extends ActionBarActivity {
         vandalTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //intent
+                Intent intent = new Intent(StartScreen.this, Vandal.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
     }
@@ -58,7 +62,9 @@ public class StartScreen extends ActionBarActivity {
         janitorTV.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                //intent
+                Intent intent = new Intent(StartScreen.this, Janitor.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
     }
