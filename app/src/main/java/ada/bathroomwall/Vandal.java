c0b8f90aa0c11efd6fab066e3dfbb182610122de
Stephanie V <stephanievillarino@gmail.com>
2015-02-11 23:33:44 -1000

    methods for buttons to choose play mode

--- app/src/main/java/ada/bathroomwall/Vandal.java
+++ app/src/main/java/ada/bathroomwall/Vandal.java
@@ -4,27 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 
-public class StartScreen extends ActionBarActivity {
+public class Vandal extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen);
-        this.playAsVandal();
-        this.playAsJanitor();
+        setContentView(R.layout.activity_vandal);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_vandal, menu);
         return true;
     }
 
@@ -41,25 +35,5 @@ public class StartScreen extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void playAsVandal(){
-        TextView vandalTV = (TextView) findViewById(R.id.textViewVandal);
-        vandalTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //intent
-            }
-        });
-    }
-
-    private void playAsJanitor() {
-        TextView janitorTV = (TextView) findViewById(R.id.textViewJanitor);
-        janitorTV.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                //intent
-            }
-        });
     }
 }
